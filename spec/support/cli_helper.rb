64be79ffd5c4cdf29c00f93334441df68abe2167
Scott Arthur <scott@scottatron.com>
2014-06-24 00:54:18 +1000

    Added cli_output_for spec helper to help with acceptance tests

--- spec/support/cli_helper.rb
+++ spec/support/cli_helper.rb
@@ -6,4 +6,9 @@ module CliHelper
     env = {'PATH' => "#{BIN}:#{ENV['PATH']}"}
     out, status = Open3.capture2e env, 'gorobotgo', stdin_data: commands
   end
+
+  def cli_output_for(commands)
+    out, status = run_cli commands
+    out
+  end
 end
